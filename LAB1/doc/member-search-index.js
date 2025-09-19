d5fac9c9ec4117daddcdc0a2fa423fd85d0bf15d
unknown <marian.hrytsan.ki.2023@lpnu.uagit>
2025-09-19 11:20:17 +0300

    Versin 1.0

---
+++ LAB1/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1HrytsanKI303","l":"Lab1HrytsanKI303()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1HrytsanKI303","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
