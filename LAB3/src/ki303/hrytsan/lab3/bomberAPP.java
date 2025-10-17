7cc5e543c913f8abffbb11e86441f6369a885da4
unknown <marian.hrytsan.ki.2023@lpnu.uagit>
2025-10-17 11:36:11 +0300

    LAB3

---
+++ LAB3/src/ki303/hrytsan/lab3/bomberAPP.java
@@ -0,0 +1,11 @@
+package ki303.hrytsan.lab3;
+
+public class bomberAPP {
+	public static void main(String[] args) 
+	{
+		bomber A= new bomber(45);
+		A.loadShells();
+		A.dropAshell();
+		A.scanning();
+	}
+}
