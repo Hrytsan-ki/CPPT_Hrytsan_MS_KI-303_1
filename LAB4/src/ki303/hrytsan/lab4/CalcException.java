8fc9caa103e76daab9752fa8e487dac69a3c95cf
unknown <marian.hrytsan.ki.2023@lpnu.uagit>
2025-11-14 22:24:31 +0200

    LAB4 version 1.0

---
+++ LAB4/src/ki303/hrytsan/lab4/CalcException.java
@@ -0,0 +1,11 @@
+	package ki303.hrytsan.lab4;
+	
+	
+	class CalcException extends  ArithmeticException
+	{
+	public CalcException(){}
+	public CalcException(String cause)
+	{
+	super(cause);
+	}
+	}
